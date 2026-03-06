b400923bff779ed581b8bcf4f0eb70c6b7aebeca
sarahdidulo <didulo.sarah@gmail.com>
2026-03-06 10:41:56 +0800

    removed strict mode

--- src/components/TransactionChart/data.js
+++
@@ -1,9 +0,0 @@
-export default function getData() {
-  return [
-    { asset: "Stocks", amount: 60000 },
-    { asset: "Bonds", amount: 40000 },
-    { asset: "Cash", amount: 7000 },
-    { asset: "Real Estate", amount: 5000 },
-    { asset: "Commodities", amount: 3000 },
-  ];
-}
--- src/main.jsx
+++ src/main.jsx
@@ -6,11 +6,9 @@ import { CurrentUserProvider } from "./components/CurrentUserContext";
 import { BannerProvider } from './components/Banner/BannerContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <StrictMode>
       <CurrentUserProvider>
         <BannerProvider>
           <App />
         </BannerProvider>
-      </CurrentUserProvider>
-    </StrictMode>,
+      </CurrentUserProvider>,
 )
