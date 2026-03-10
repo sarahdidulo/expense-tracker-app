8d4b03ef8dee43e4474d5a5688f61adfe3699d73
sarahdidulo <didulo.sarah@gmail.com>
2026-03-10 10:46:45 +0800

    modifications

--- src/components/Overview/Overview.jsx
+++ src/components/Overview/Overview.jsx
@@ -1,15 +1,17 @@
 import { useContext, useEffect, useState } from "react";
 import { Navigate } from "react-router-dom";
-import TransactionChart from "../TransactionChart/TransactionChart.jsx"
 import { CurrentUserContext } from "../CurrentUserContext";
 
 export default function Overview() {
 
+
+    
     return (
         <>
             <h1>Overview Content</h1>
-            <TransactionChart/>
+            
         </>
  
     );
 }
+
